ced6849d583751434ab01c95061e155a4bf863b2
Juan Andres Vivas <juan.vivas16@gmail.com>
2016-04-02 21:55:12 -0430

    prueba login con try catch

--- src/main_io/Controller.java
+++ src/main_io/Controller.java
@@ -10,34 +10,39 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Controller
-{
-    @FXML private TextField user_text_field;
-    @FXML private TextField pass_text_field;
-    @FXML private Db_connection db = new Db_connection();
-    @FXML private Connection con;
+public class Controller {
+    @FXML
+    private TextField user_text_field;
+    @FXML
+    private TextField pass_text_field;
+    @FXML
+    private Db_connection db = new Db_connection();
+    @FXML
+    private Connection con;
 
-    @FXML protected void handle_login_button_action (ActionEvent event) throws SQLException
-    {
-        System.out.println("clicked Entrar");
-        con = db.get_connection();
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select name,pass from Person p, User u where p.id=u.id");
-        //    ResultSet ps = st.executeQuery("select pass from User");
+    @FXML
+    protected void handle_login_button_action(ActionEvent event){ //throws SQLException {
+        try {
+            System.out.println("clicked Entrar");
+            con = db.get_connection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select name,pass from Person p, User u where p.id=u.id");
+            //    ResultSet ps = st.executeQuery("select pass from User");
 
-        while (rs.next()) {
-            String name = rs.getString("name");
-            String pass = rs.getString("pass");
-            System.out.println("name: " + name);
-            System.out.println("field: " + user_text_field.getText());
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String pass = rs.getString("pass");
+                System.out.println("name: " + name);
+                System.out.println("field: " + user_text_field.getText());
 
-            if (user_text_field.getText().equals(name) && pass_text_field.getText().equals(pass))
-                System.out.println("User name registered");
+                if (user_text_field.getText().equals(name) && pass_text_field.getText().equals(pass))
+                    System.out.println("User name registered");
 
+            }
+        } catch (SQLException err) {
+            System.out.println(err);
         }
+        //user_text_field.getText()
+        //pass_text_field.getText()
     }
-    //user_text_field.getText()
-    //pass_text_field.getText()
-
-
 }
